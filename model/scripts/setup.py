845617c1cda8130f0f57028592e1192cb5e22124
andreag186 <amgstar86@gmail.com>
2024-03-04 14:44:25 +1300

    set up models

---
+++ model/scripts/setup.py
@@ -0,0 +1,17 @@
+#SETUP
+import math
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import random
+import copy
+import numpy as np
+import sys
+
+from scipy import stats
+from scipy.stats import norm
+from scipy.stats import qmc
+
+import itertools
+
+import csv
+import pandas as pd
